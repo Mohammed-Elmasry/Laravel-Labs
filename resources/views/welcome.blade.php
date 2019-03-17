cc664583b891d8432320403b7c7d4ffbe8499e69
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-03-17 23:01:28 +0200

    validate display of all posts based on authentication of user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,11 +67,12 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
+                    @auth <!--If user is authenticated-->
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
+                        <a href="{{route('posts.index')}}">All posts</a>
+                    @else <!--if user is not authenticated-->
                         <a href="{{ route('login') }}">Login</a>
-
+                        <a href="{{route('login')}}">All posts</a>
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
