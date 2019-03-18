739a6fba9d34926ce75bbd367305cb1ce78f06b3
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-03-18 12:46:17 +0200

    adding code body to update resource controller

--- app/Http/Controllers/PostImagesController.php
+++ app/Http/Controllers/PostImagesController.php
@@ -13,7 +13,6 @@ class PostImagesController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -23,62 +22,67 @@ class PostImagesController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $path = $request->file('avatar')->store('avatars');
+
+        return $path;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $path = $request->file('avatar')->store('avatars');
+
+        return $path;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
     }
 }
